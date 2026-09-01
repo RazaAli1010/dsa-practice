1d5105cc4f8190c2065bcc6fd27d53b57e85a81a
Raza Ali <razaali2461357@gmail.com>
2026-09-01 14:24:55 +0500

    remove all adjacent duplicates

---
+++ leetcode/remove_all_adjacent.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def removeDuplicates(self, s):
+        stack=[]
+        for ch in s:
+            if stack and ch==stack[-1]:
+                stack.pop()
+            else:
+                stack.append(ch)
+        return ''.join(stack)
+
+                
+        
+        
