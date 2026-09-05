4c9795d80eaec263a18e4aaf44d511d9d34f4e5c
Raza Ali <razaali2461357@gmail.com>
2026-09-05 17:14:42 +0500

    ransomNote and magazine

---
+++ leetcode/hashmap_ransomnote.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        if len(ransomNote)>len(magazine):
+            return False
+        freq={}
+
+        for ch in magazine:
+            freq[ch]=freq.get(ch,0)+1
+        for ch in ransomNote:
+            if freq.get(ch,0)<=0:
+                return False
+            freq[ch]-=1
+        return True
+        
